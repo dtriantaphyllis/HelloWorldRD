1c6b4d54286d1725115d716d2fa2f012f9ae0037
dtriantaphyllis <57912121+dtriantaphyllis@users.noreply.github.com>
2019-11-20 15:23:00 -0600

    eclipse supremacy proved

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,8 @@ public class Main {
 
 	//this code was garbage
 	//this is added from a different Branch
+	public static void main(String args[])
+	{
+		System.out.println("test this bad boi");
+	}
 }
